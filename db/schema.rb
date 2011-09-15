45a1dd5960efeeb2f77980780d403a212e10ccd9
Jhanboy Abelgas <blasiong@yahoo.com.ph>
2011-09-15 10:29:03 +0800

    User model with passwords, and user show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110915014944) do
+ActiveRecord::Schema.define(:version => 20110915021312) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(:version => 20110915014944) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
 end
